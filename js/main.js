0cdd0423db27436319566ca689903c0e252e86f5
Vitalii <witaliy_sapriga@ukr.net>
2022-03-22 09:34:11 +0200

    Added gallery section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+$(function () {
+  let mixer = mixitup('.gallery__content');
+  $('.gallery__filter-btn').on('click', function () {
+    $('.gallery__filter-btn').removeClass('gallery__filter-btn--active');
+    $(this).addClass('gallery__filter-btn--active');
+  });
+});
