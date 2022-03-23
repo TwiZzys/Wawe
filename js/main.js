2495cbaff305738e0f27219b199ac1aaebeb42f8
Vitalii <witaliy_sapriga@ukr.net>
2022-03-23 09:23:27 +0200

    Added blog section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(function () {
-  let mixer = mixitup('.gallery__content');
+
+  $('.blog-slider').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 1500
+  })
+
   $('.gallery__filter-btn').on('click', function () {
     $('.gallery__filter-btn').removeClass('gallery__filter-btn--active');
     $(this).addClass('gallery__filter-btn--active');
   });
+  
+  let mixer = mixitup('.gallery__content');
 });
