f97baa10b98d8633ad09c5cdf9c993218d2623a8
Vitalii <witaliy_sapriga@ukr.net>
2022-04-03 14:57:25 +0300

    Added smooth scroll and add to top button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,45 @@
 $(function () {
+  // при нажатии на кнопку scrollup
+  $('.toTop').on('click', function (e) {
+    e.preventDefault();
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate({
+      scrollTop: 0
+    }, 1000);
+  })
+  $('.menu').on('click', 'a', function (e) {
+    e.preventDefault();
+    let cl = $(this).attr('href'),
+      top = $(cl).offset().top;
 
-  $('.menu__btn').on('click', function() {
+    $('html,body').animate({
+      scrollTop: top
+    }, 1500)
+  });
+  $('.header__icon').on('click', function (e) {
+    e.preventDefault();
+
+    let cl = $(this).attr('href'),
+    top = $(cl).offset().top;
+
+    $('html,body').animate({
+      scrollTop: top
+    }, 1000)
+  })
+  // при прокрутке окна (window)
+  $(window).scroll(function () {
+    // если пользователь прокрутил страницу более чем на 200px
+    if ($(this).scrollTop() > 500) {
+      // то сделать кнопку scrollup видимой
+      $('.toTop').fadeIn();
+    }
+    // иначе скрыть кнопку scrollup
+    else {
+      $('.toTop').fadeOut();
+    }
+  });
+
+  $('.menu__btn').on('click', function () {
     $('.menu__list').toggleClass('menu__list--active');
   });
 
@@ -16,6 +55,6 @@ $(function () {
     $('.gallery__filter-btn').removeClass('gallery__filter-btn--active');
     $(this).addClass('gallery__filter-btn--active');
   });
-  
+
   let mixer = mixitup('.gallery__content');
 });
